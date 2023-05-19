7af9df80225645addcea1c9b0e4942e381b979c8
Peter <peetdrum@gmail.com>
2023-05-19 12:24:46 +0200

    Huiswerkopgaven les 8 uitgewerkt met algemene_functies en reclame bestanden

---
+++ algemene_functies.py
@@ -0,0 +1,5 @@
+def mijn_functie_1(a):
+    return pow(a,2)
+
+def mijn_functie_2(b,c):
+    return [int(b+c), int(b-c), int(b*c), int(b/c)]
---
+++ reclame.py
@@ -0,0 +1,23 @@
+from algemene_functies import mijn_functie_2
+import statistics
+
+
+def aanbieding_1(smaak, prijs, korting):
+   return f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {prijs - (prijs * korting):.2f} euro."
+
+def inkomsten_totaal(inkomsten, btw):
+   return f"Het totaal van alle inkomsten van deze week is {sum(inkomsten)} euro, waarover {sum(inkomsten)*btw:.2f} euro btw betaald dient te worden."
+
+def laag_en_hoog(mijn_lijst):
+   return [max(mijn_lijst), min(mijn_lijst)]
+
+def gemiddelde(mijn_lijst):
+   return f"De gemiddelde inkomsten deze week zijn {statistics.mean(mijn_lijst):.2f} euro"
+
+def meervoudig(invoer_lijst):
+   return laag_en_hoog(invoer_lijst)
+
+def combinatie(invoer_lijst_2):
+   korte_lijst = laag_en_hoog(invoer_lijst_2)
+   uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
+   return uitvoer
