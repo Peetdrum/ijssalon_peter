c0f0bed8f647bc7e36dc47cee34924435566fcda
Peter <peetdrum@gmail.com>
2023-05-21 19:19:24 +0200

    Correcties huiswerk

--- reclame.py
+++ reclame.py
@@ -24,5 +24,3 @@ def combinatie(invoer_lijst_2):
    korte_lijst = laag_en_hoog(invoer_lijst_2)
    uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
    return uitvoer
-
-print(gemiddelde([10,5,5]))
